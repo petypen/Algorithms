3e632146903d71c2a5acaf1ea97352fb578b4268
pen <petypen@yandex.ru>
2021-06-19 21:21:21 +0300

    feature: ArrayGenerator - AsOrdered Генератор упорядоченного массива

---
+++ GrokkingAlgorithms/ArrayGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class ArrayGenerator
+    {
+        internal static int[] AsOrdered(uint arraySize)
+        {
+            // magic numeric
+            // https://docs.microsoft.com/en-us/dotnet/api/system.array?redirectedfrom=MSDN&view=net-5.0
+            const int maxSizeArray = 0X7FEFFFFF;
+
+            int[] ArrayResult;
+
+            switch (arraySize)
+            {
+                case 0:
+                    ArrayResult = new int[1];
+                    break;
+                case < maxSizeArray:
+                    ArrayResult = new int[arraySize];
+                    break;
+                default:
+                    ArrayResult = new int[maxSizeArray];
+                    arraySize = maxSizeArray;
+                    break;
+            }
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                ArrayResult[i] = i;
+            }
+
+            return ArrayResult;
+        }
+
+    }
+}
--- GrokkingAlgorithms/Program.cs
+++ GrokkingAlgorithms/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace GrokkingAlgorithms
+namespace Algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] TestSet = ArrayGenerator.AsOrdered(int.MaxValue);
+            Console.WriteLine(TestSet.Length);
         }
     }
 }
