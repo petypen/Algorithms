07100633b1dbf7cfeab4108303fcc2483ea149f2
pen <petypen@yandex.ru>
2021-06-20 13:46:36 +0300

    test: тесты для всех случаев
    -- отработаны крайгие события

--- GrokkingAlgorithmsTests/ArrayGeneratorTests.cs
+++ GrokkingAlgorithmsTests/ArrayGeneratorTests.cs
@@ -17,11 +17,46 @@ namespace GrokkingAlgorithmsTests
             testArray = ArrayGenerator.AsOrdered(arraySize);
 
             // assert
-            Assert.Equal<int>(testArray.Length, (int)arraySize);
+            Assert.Equal<int>((int)arraySize, testArray.Length);
             for (int i = 0; i < arraySize; i++)
             {
                 Assert.Equal<int>(testArray[i], i);
             }
         }
+
+        [Fact]
+        public void AsOrder_0()
+        {
+            // arrange
+            uint arraySize = 0;
+            int[] testArray;
+
+            // act
+            testArray = ArrayGenerator.AsOrdered(arraySize);
+
+            // assert
+            Assert.Single(testArray);
+            Assert.Equal<int>(0, testArray[0]);
+        }
+
+        [Fact]
+        public void AsOrder_Max()
+        {
+            // arrange
+            uint arraySize = uint.MaxValue;
+            int[] testArray;
+            const int maxSizeArray = 0X7FEFFFFF;
+
+            // act
+            testArray = ArrayGenerator.AsOrdered(arraySize);
+
+            // assert
+            Assert.Equal(maxSizeArray, testArray.Length);
+            for (int i = 0; i < maxSizeArray; i++)
+            {
+                Assert.Equal<int>(testArray[i], i);
+            }
+        }
+
     }
 }
