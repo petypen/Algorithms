ef034dc183f31a4835475754422314a65f7b533e
pen <petypen@yandex.ru>
2021-06-21 21:49:38 +0300

    refactor: удобный вывод отладочной информации

--- GrokkingAlgorithms/Program.cs
+++ GrokkingAlgorithms/Program.cs
@@ -7,8 +7,15 @@ namespace GrokkingAlgorithms
         static void Main()
         {
             Console.WriteLine("Hello World!");
-            int[] TestSet = ArrayGenerator.AsOrdered(int.MaxValue);
-            Console.WriteLine(TestSet.Length);
+            int[] TestSet = ArrayGenerator.AsOrdered(0);
+            ArrayGenerator.AsMixed(ref TestSet);
+
+            foreach (var item in TestSet)
+            {
+                Console.Write("{0, 5}", item);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Number of elements {TestSet.Length}");
         }
     }
 }
