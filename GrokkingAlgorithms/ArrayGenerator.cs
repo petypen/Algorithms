7fc3d68733f9873b95d8bc194d2aea4c441d8cfb
pen <petypen@yandex.ru>
2021-06-21 20:16:08 +0300

    refactor: удален неиспользуемый метод

--- GrokkingAlgorithms/ArrayGenerator.cs
+++ GrokkingAlgorithms/ArrayGenerator.cs
@@ -8,37 +8,7 @@ namespace GrokkingAlgorithms
 {
     public static class ArrayGenerator
     {
-        public static int[] AsOrdered(uint arraySize)
-        {
-            // magic numeric
-            // https://docs.microsoft.com/en-us/dotnet/api/system.array?redirectedfrom=MSDN&view=net-5.0
-            const uint maxSizeArray = 0X7FEFFFFF;
-
-            int[] ArrayResult;
-
-            switch (arraySize)
-            {
-                case 0:
-                    arraySize = 1;
-                    break;
-                case > maxSizeArray:
-                    arraySize = maxSizeArray;
-                    break;
-                default:
-                    break;
-            }
-
-            ArrayResult = new int[arraySize];
-
-            for (int i = 0; i < arraySize; i++)
-            {
-                ArrayResult[i] = i;
-            }
-
-            return ArrayResult;
-        }
-
-        public static int[] AsOrdered(int arraySize)
+         public static int[] AsOrdered(int arraySize)
         {
             // magic numeric
             // https://docs.microsoft.com/en-us/dotnet/api/system.array?redirectedfrom=MSDN&view=net-5.0
