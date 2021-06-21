ceaea2c38a1ee642b63d8e5883d7b89baaf02d41
pen <petypen@yandex.ru>
2021-06-21 21:48:58 +0300

    feature: AsMixed перемешивание массива
    -- метод перетасовки массива по алгоритму Тасование Фишера-Йетса

--- GrokkingAlgorithms/ArrayGenerator.cs
+++ GrokkingAlgorithms/ArrayGenerator.cs
@@ -8,7 +8,7 @@ namespace GrokkingAlgorithms
 {
     public static class ArrayGenerator
     {
-         public static int[] AsOrdered(int arraySize)
+        public static int[] AsOrdered(int arraySize)
         {
             // magic numeric
             // https://docs.microsoft.com/en-us/dotnet/api/system.array?redirectedfrom=MSDN&view=net-5.0
@@ -29,5 +29,22 @@ namespace GrokkingAlgorithms
             return Enumerable.Range(0, arraySize).ToArray();
         }
 
+        public static void AsMixed(ref int[] orderedArray)
+        {
+            Random random = new();
+
+            int element = orderedArray.Length;
+            while (element > 1)
+            {
+                int otherElement = random.Next(element);
+                int otherElementValue = orderedArray[otherElement];
+
+                element--;
+
+                orderedArray[otherElement] = orderedArray[element];
+                orderedArray[element] = otherElementValue;
+            }
+        }
     }
 }
+
