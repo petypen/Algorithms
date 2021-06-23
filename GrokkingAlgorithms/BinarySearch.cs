2f3def83dfc17ae95937ad31b779ff05c220a321
pen <petypen@yandex.ru>
2021-06-23 21:37:58 +0300

    feature: алгоритм бинарного поиска, начало реализации

---
+++ GrokkingAlgorithms/BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrokkingAlgorithms
+{
+    public static class BinarySearch
+    {
+        public static int? SearchOne(int[] sortedList, int item)
+        {
+            int low = 0;
+            int high = sortedList.Length - 1;
+            int mid;
+
+
+            mid = (high - low) / 2;
+            if (sortedList[mid] == item)
+            {
+                return mid;
+            }
+            if (sortedList[mid] > item)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+
+            return null;
+        }
+    }
+}
